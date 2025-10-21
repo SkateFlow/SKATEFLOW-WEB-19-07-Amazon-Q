823f4f06ffae29662b4d4fd4689173086e08eeca
SkateFlow <skateflowG5@gmail.com>
2025-10-21 18:38:03 -0300

    7.1,5

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração base para todas as requisições axios
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https//:localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
